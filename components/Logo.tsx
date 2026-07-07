6ae9260f507003464e95a522c8aa6adb8db21ea8
Дмитрий Молчан <dmitriymolchan@MacBook-Air-Dmitrij.local>
2026-07-07 18:20:43 +0300

    Fix header logo size

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,9 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export function Logo() {
   return (
     <Link className="logo" href="/" aria-label="Solvix">
-      <img className="logo-image" src="/solvix-logo-wordmark.png" alt="" aria-hidden="true" />
+      <Image
+        className="logo-image"
+        src="/solvix-logo-wordmark.png"
+        alt=""
+        width={512}
+        height={512}
+        priority
+        aria-hidden="true"
+      />
     </Link>
   );
 }
