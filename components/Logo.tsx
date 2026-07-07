4ce27a43079e1f380b6f05ca68e47c8c19b5a412
Дмитрий Молчан <dmitriymolchan@MacBook-Air-Dmitrij.local>
2026-07-07 18:13:30 +0300

    Update site logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,10 +3,7 @@ import Link from "next/link";
 export function Logo() {
   return (
     <Link className="logo" href="/" aria-label="Solvix">
-      <span className="logo-wordmark" aria-hidden="true">
-        <span className="logo-word-main">Solvi</span>
-        <span className="logo-x-letter">X</span>
-      </span>
+      <img className="logo-image" src="/solvix-logo-wordmark.png" alt="" aria-hidden="true" />
     </Link>
   );
 }
